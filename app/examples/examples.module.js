e0519c4e4f50be283cde2640f10ddc084407eb79
jpathak2 <pathakj@hotmail.com>
2016-02-08 11:39:21 -0800

    v 1.7

--- app/examples/examples.module.js
+++ app/examples/examples.module.js
@@ -1,12 +1,11 @@
 (function() {
     'use strict';
-
     angular
         .module('app.examples', [
             //'app.examples.authentication',
             //'app.examples.calendar',
             //'app.examples.charts',
-            'app.examples.dashboards',
+           // 'app.examples.dashboards',
             //'app.examples.elements',
             //'app.examples.email',
            // 'app.examples.extras',
